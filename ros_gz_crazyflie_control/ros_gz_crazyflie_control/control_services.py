b8586ef1b7b783d0bc3b52d2f3ff7b6fe95967b2
knmcguire <kimberly@bitcraze.io>
2024-08-25 14:20:14 +0200

    unified scan and odometry topics

--- ros_gz_crazyflie_control/ros_gz_crazyflie_control/control_services.py
+++ ros_gz_crazyflie_control/ros_gz_crazyflie_control/control_services.py
@@ -20,7 +20,7 @@ class ControlServices(Node):
         max_ang_z_rate = self.get_parameter('max_ang_z_rate').value
 
         self.publisher_ = self.create_publisher(Twist, robot_prefix + incoming_twist_topic, 10)
-        self.subscriber = self.create_subscription(Odometry, robot_prefix + '/odometry', self.odometry_callback, 10)
+        self.subscriber = self.create_subscription(Odometry, robot_prefix + '/odom', self.odometry_callback, 10)
         self.subscriber = self.create_subscription(Twist, incoming_twist_topic, self.cmd_vel_callback, 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
 
